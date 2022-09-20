28659bd25fc7bd36bfc8eaee25152911467c7878
unknown <sfjbsspet@gmail.com>
2022-09-20 21:26:09 +0530

    this is updated commit

--- NodeJS/hellow-world-app/index.js
+++ NodeJS/hellow-world-app/index.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 function greet(req,res){
-    res.write("<h1>Hey this is Kishan.</h1>");
+    res.write("<h1>Hey this is Kishan. There am i </h1>");
     res.end();
 }
 
