2ce62c3c9642c9c339c5a478399b1feaf2d620ff
unknown <sfjbsspet@gmail.com>
2022-09-20 20:19:04 +0530

    sample nodejs

---
+++ NodeJS/hellow-world-app/index.js
@@ -0,0 +1,9 @@
+//console.log("Hello this is Kishan");
+const http = require('http');
+
+function greet(req,res){
+    res.write("<h1>Hey this is Kishan.</h1>");
+    res.end();
+}
+
+http.createServer(greet).listen(3000);
